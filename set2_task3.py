57477c69aec97a872f2892911fc792d34ee71d88
Helge Fredriksen <helgeingvart@gmail.com>
2023-09-04 21:46:15 +0200

    Work on set 2

--- set2_task3.py
+++ set2_task3.py
@@ -14,13 +14,19 @@ N_2 = multivariate_normal([1,1], cov=1)
 
 bias = np.ones((3,1))
 X_1 = N_1.rvs(3)
-X_1 = np.hstack((bias, X_1))
 X_2 = N_2.rvs(3)
+
+plt.scatter(X_1[:,0], X_1[:,1], color="red")
+plt.scatter(X_2[:,0], X_2[:,1], color="blue")
+x =np.linspace(-2,2, 50)
+y = 1 - x    # Optimum regression line
+plt.plot(x,y, color="green")
+
+X_1 = np.hstack((bias, X_1))
 X_2 = np.hstack((bias, X_2))
 
 # Suggested parameters for decision boundary. Here w[0] is the constant w_0
 w = np.array([-1, 1, 1])
-# w = np.array([-1, 1, 1])
 
 z_1 = sigmoid(X_1, w)
 z_2 = sigmoid(X_2, w)
@@ -31,3 +37,5 @@ ax.scatter(X_1[:,0], X_1[:,1], z_1, color = "green")
 ax.scatter(X_2[:,0], X_2[:,1], z_2, color = "red")
 
 plt.show()
+
+#%%
